d058bf09f7a28f362007439b26a7c9f9dc6c1db6
Matt Venn <matt@mattvenn.net>
2023-03-01 19:08:37 +0100

    wait 1000

--- src/tb.v
+++ src/tb.v
@@ -26,7 +26,7 @@ module tb (
     assign segments = outputs[6:0];
 
     // instantiate the DUT
-    seven_segment_seconds #(.MAX_COUNT(100)) seven_segment_seconds(
+    seven_segment_seconds seven_segment_seconds(
         `ifdef GL_TEST
             .vccd1( 1'b1),
             .vssd1( 1'b0),
